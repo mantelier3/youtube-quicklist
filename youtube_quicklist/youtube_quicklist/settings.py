59ceba72059fdb1164eaa9859239b3d2b81f447c
Kristjan Ribolica <riba1122@gmail.com>
2016-05-11 22:20:36 +0200

    added local settings and changed timezone

--- youtube_quicklist/youtube_quicklist/settings.py
+++ youtube_quicklist/youtube_quicklist/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'quicklist.apps.QuicklistConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Ljubljana'
 
 USE_I18N = True
 
@@ -119,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from local_settings import *
+except ImportError:
+    pass  # No local_settings file
