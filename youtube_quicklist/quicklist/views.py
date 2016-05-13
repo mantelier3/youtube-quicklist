37a681b26c1d1fa526c4a5012f0d2d88793e4b4e
Kristjan Ribolica <riba1122@gmail.com>
2016-05-13 23:50:58 +0200

    removed hardcoded query and set it from GET method

--- youtube_quicklist/quicklist/views.py
+++ youtube_quicklist/quicklist/views.py
@@ -8,8 +8,9 @@ from django.conf import settings
 
 def index(request):
     service = build('youtube', 'v3', developerKey=settings.API_KEY)
-    response = service.search().list(q="clio goes two wheel", part="id", maxResults=5).execute()
     cond = 5
+    query = request.GET.get('query','clio goes two wheel')
+    response = service.search().list(q=query, part="id", maxResults=5).execute()
     videoId = response["items"][0]["id"]["videoId"]
-    context = { 'cond':cond, 'videoId':videoId }
+    context = { 'cond':cond, 'videoId':videoId, 'query':query}
     return render(request, 'quicklist/index.html', context)
