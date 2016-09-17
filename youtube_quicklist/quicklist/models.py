dd4da7e95983066fbb33eea9dd51e778c29d6d7b
Kristjan Ribolica <riba1122@gmail.com>
2016-09-17 19:41:48 +0200

    introduced models for User, Playlist, Results, and Video

--- youtube_quicklist/quicklist/models.py
+++ youtube_quicklist/quicklist/models.py
@@ -3,3 +3,24 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    session_id = models.CharField(max_length=120)
+
+class Playlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    playlist_name = models.CharField(max_length=20)
+    # @classmethod
+    # def create(cls, name, email):
+    #     return cls(name=name, email=email)
+
+class Results(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    results_name = models.CharField(max_length=20)
+
+class Video(models.Model):
+    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE, blank=True, null=True)
+    results = models.ForeignKey(Results, on_delete=models.CASCADE, blank=True, null=True)
+    video_id = models.CharField(max_length=20)
+    thumbnail_url = models.CharField(max_length=120)
+    # video_url = models.CharField(max_length=120)
