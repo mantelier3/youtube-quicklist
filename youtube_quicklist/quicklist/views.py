8d8db9b86eb9d982e858d3956cf864c3d4e771fe
Kristjan Ribolica <riba1122@gmail.com>
2016-09-17 19:43:50 +0200

    cleaned up views

--- youtube_quicklist/quicklist/views.py
+++ youtube_quicklist/quicklist/views.py
@@ -1,20 +1,48 @@
-from django.shortcuts import render
+from .models import User, Playlist, Results, Video
 
 from django.http import HttpResponse
 from django.template import loader
-from django.shortcuts import render
-from googleapiclient.discovery import build
+from django.shortcuts import render, redirect
 from django.conf import settings
+from django.core.urlresolvers import reverse
+from .helpers import * # need to change so it doesn't contain app name - tincek fixed
 
 def index(request):
-    service = build('youtube', 'v3', developerKey=settings.API_KEY)
-    cond = 5
+    playlist = get_or_create_playlist(request)
     query = request.GET.get('query','clio goes two wheel')
-    response = service.search().list(q=query, part="id, snippet", type="video", maxResults=5).execute()
-    videoId = response["items"][0]["id"]["videoId"]
-
-    results = [ { "videoId":item["id"]["videoId"], "url":item["snippet"]["thumbnails"]["default"]["url"] } for item in response["items"] ]
-
-
-    context = { 'cond':cond, 'videoId':videoId, 'query':query, 'results':results}
+    results = search_videos(query)
+    request.session["results"] = results
+    context = { 'playlist': playlist, 'video_id':results[0]["video_id"],
+                'results':results }
+    #return HttpResponse(playlist)
     return render(request, 'quicklist/index.html', context)
+
+def add(request):
+    rs = request.session
+    video_id = int(request.GET.get("video_id"))
+    playlist = get_or_create_playlist(request)
+    #playlist.append({'thumbnail_url': })
+    playlist.append(rs["results"][video_id])
+    request.session['playlist'] = playlist
+    # request.session['playlist'] = []
+    return redirect(reverse("index"))
+    # return HttpResponse(request.GET.get("video_id"))
+    # return HttpResponse(', '.join(playlist))
+
+def remove(request):
+    playlist = get_or_create_playlist(request)
+    playlist.remove(request.GET.get("video_id")) # will error if video_id is not in playlist
+    # request.session['playlist'] = playlist # clear playlist
+    return redirect(reverse("index"))
+
+def asdf(request):
+    if 'count' in request.session:
+        request.session['count'] += 1
+        return HttpResponse('new count=%s' % request.session['count'])
+    else:
+        request.session['count'] = 1
+        return HttpResponse('No count in session. Setting to 1')
+
+def clear_playlsit(request):
+    del request.session["playlist"]
+    return redirect(reverse("index"))
