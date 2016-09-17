a9f9754dc1bea694fd9e407ee53f7ac2f2f4badd
Kristjan Ribolica <riba1122@gmail.com>
2016-09-17 19:43:08 +0200

    renamed urlconf

--- youtube_quicklist/quicklist/urls.py
+++ youtube_quicklist/quicklist/urls.py
@@ -1,8 +1,20 @@
-from django.conf.urls import url
-
+from django.conf.urls import include, patterns, url
 from . import views
+from django.conf import settings
+
 
 urlpatterns = [
     #url(r'^$', views.index, name='index'),
-    url(r'^$', views.index, name=''),
+    url(r'^$', views.index, name='index'),
+    url(r'^index$', views.index, name=''),
+    url(r'^add$', views.add, name=''),
+    url(r'^remove$', views.remove, name=''),
+    url(r'^asdf$', views.asdf, name=''),
+    url(r'^clear_playlist$', views.clear_playlsit, name=''),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
