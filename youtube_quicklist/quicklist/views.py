38d90d7ec9c31dbedcb5aab10fda260b697d08c8
Kristjan Ribolica <riba1122@gmail.com>
2016-09-26 03:26:04 +0200

    implemented ajax search and playlist updating

--- youtube_quicklist/quicklist/views.py
+++ youtube_quicklist/quicklist/views.py
@@ -5,26 +5,48 @@ from django.template import loader
 from django.shortcuts import render, redirect
 from django.conf import settings
 from django.core.urlresolvers import reverse
+from django.template.loader import render_to_string
 from .helpers import *
+import json
+
+# def ajax_test(request):
+#     return HttpResponse("foo")
+def ajax_test(request):
+    pass
+    if not "asdf" in request.session:
+        request.session["asdf"] = 1
+    else:
+        request.session["asdf"] += 2
+    test_data = {'a':'aaa','b':'bbb','c':'ccc'}
+    a = json.dumps(test_data)
+    return HttpResponse(a)
 
 def index(request):
+    query = request.GET.get("query", False)
+    if query:
+        results = search_videos(query)
+        request.session["results"] = results
+        if request.is_ajax():
+            if request.GET["need_html_results"] == "true":
+                results = render_to_string("quicklist/search_results.html", 
+                                            context=None, request=request)
+            response_data = {"results": results}
+            return HttpResponse(json.dumps(response_data))
     current_video_id = ""
     playlist_exists = False
-    print("PLAYLIST IS FALSE")
     if "current_video_index" not in request.session:
         request.session["current_video_index"] = 0
     playlist = get_or_create_playlist(request)
     print(playlist)
     if len(playlist) > 0:
         playlist_exists = True
-        print("PLAYLIST IS TRUE I MEAN IT'S NOT EMPTY IT ACTUALLY EXISTS")
-        current_video_id = playlist[request.session["current_video_index"]]["video_id"]
-    query = request.GET.get("query", False)
-    if query:
-        results = search_videos(query)
-        request.session["results"] = results
+        current_video_id = playlist[request.session["current_video_index"]] \
+                                                   ["video_id"]
+    
+
     request.session["playlist"] = playlist
-    context = { 'playlist': playlist, 'current_video_id': current_video_id, 'playlist_exists': playlist_exists }
+    context = { 'playlist': playlist, 'current_video_id': current_video_id,
+                'playlist_exists': playlist_exists }
     return render(request, 'quicklist/index.html', context)
 
 def play_next(request):
@@ -32,22 +54,27 @@ def play_next(request):
 
 def add(request):
     rs = request.session
-    video_id = int(request.GET.get("video_id"))
+    add_video_index = int(request.GET["add_video_index"])
     playlist = get_or_create_playlist(request)
-    playlist.append(rs["results"][video_id])
-    request.session['playlist'] = playlist
-    return redirect(reverse("index"))
+    rs['playlist'].append(rs["results"][add_video_index])
+    rendered_playlist = render_to_string("quicklist/playlist.html", 
+                                     context=None, request=request)
+    return HttpResponse(json.dumps(rendered_playlist))
+    # return redirect(reverse("index"))
 
 def next_video(request):
     request.session["current_video_index"] += 1
-    if request.session["current_video_index"] >= len(request.session["playlist"]):
+    if request.session["current_video_index"] >= \
+       len(request.session["playlist"]):
         request.session["current_video_index"] = 0
     return redirect(reverse("index"))
 
 
 def remove(request):
     playlist = get_or_create_playlist(request)
-    playlist.remove(request.GET.get("video_id")) # will error if video_id is not in playlist
+    item_for_removal = int(request.GET["remove_video_index"])
+    del playlist[item_for_removal]
+    request.session['playlist'] = playlist
     # request.session['playlist'] = playlist # clear playlist
     return redirect(reverse("index"))
 
