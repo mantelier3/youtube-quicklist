7bf97d3922249ea28a56092e4877344473ecacb9
Kristjan Ribolica <riba1122@gmail.com>
2016-10-03 21:08:31 +0200

    fixed code syntax

--- youtube_quicklist/quicklist/static/quicklist/main.js
+++ youtube_quicklist/quicklist/static/quicklist/main.js
@@ -17,6 +17,24 @@ $(document).ready(function () {
             }
         });
     });
+
+
+
+
+
+
+    //move js from template to here
+    //fix positioning when there is no video
+
+
+
+
+
+
+
+
+
+
     // Search - query youtbe for search results and display new results
     $("#form_search").on("submit", function (event) {
         event.preventDefault();
@@ -30,9 +48,9 @@ $(document).ready(function () {
                 //$("#test_p").text(results);
                 js_results = JSON.parse(results).results;
                 if (need_html_results) {
-                    $("#div_results_container").append(js_results);
-                }
-                else {
+                    $("#form_add").append(js_results);
+                    mark_buttons_as_clicked_when_clicked();
+                } else {
                     thumbnail_urls = js_results.map(function (js_results) {
                         return js_results.thumbnail_url;
                     });
@@ -43,24 +61,85 @@ $(document).ready(function () {
             }
         });
     });
-    $(".btn_add").click(function () {
-        $(this).data("clicked", true);
-    });
+
+    ajax_form_add_video();
+    mark_buttons_as_clicked_when_clicked();
+    // console.log("foo")
+    // display_video_player();
+    // console.log("bar")
     // Add - add video to playlist
+
+});
+
+function ajax_form_add_video() {
     $("#form_add").on("submit", function (event) {
+        console.log("form add submit ajax")
         event.preventDefault();
         my_event = event;
-        video_index = $(".btn_add").filter( function () {
-            return $(this).data("clicked") === true 
-        } ).val()
+        add_video_index = $(".btn_add").map( function (i, el) { 
+            if ($(el).data("clicked")) { return i } 
+        });
+        console.log(add_video_index);
+        if (add_video_index.length != 1) {
+            alert("error, more or less than one button clicked, but not one")
+        }
         $(".btn_add").data("clicked", false);
+        console.log("foobar1234")   
         $.ajax({
             url: "/quicklist/add",
-            data: {add_video_index: video_index},
+            data: {add_video_index: add_video_index[0]},
             success: function (results) {
                 js_playlist = JSON.parse(results);
-                $("#form_playlist").append(js_playlist)
+                $("#form_playlist").append(js_playlist);
+
             }
         });
     });
-});
+}
+
+function mark_buttons_as_clicked_when_clicked() {
+    $(".btn_add").click(function () {
+        $(this).data("clicked", true);
+    });
+}
+
+// function display_video_player(){
+//     //2. This code loads the IFrame Player API code asynchronously.
+//     var tag = document.createElement('script');
+//     console.log("asdf")
+//     tag.src = "https://www.youtube.com/iframe_api";
+//     var firstScriptTag = document.getElementsByTagName('script')[0];
+//     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+//     //$("div#player").append(tag);
+
+//     // 3. This function creates an <iframe> (and YouTube player)
+//     //    after the API code downloads.
+//     var player;
+//     function onYouTubeIframeAPIReady() {
+//         player = new YT.Player('player', {
+//             height: '200', // 390
+//             width: '320', // 640
+//             videoId: "-tL91n51K9k",
+//             events: {
+//             //'onReady': onPlayerReady,
+//             'onStateChange': onPlayerStateChange
+//             }
+//         });
+//     }
+
+//     // 4. The API will call this function when the video player is ready.
+//     function onPlayerReady(event) {
+//         event.target.playVideo();
+//     }
+
+//     // 5. The API calls this function when the player's state changes.
+//     var done = false;
+//     function onPlayerStateChange(event) {
+//         if (event.data == 0) {
+//             window.location.href = "next_video"
+//         }
+//     }
+//     function stopVideo() {
+//         player.stopVideo();
+//     }
+// }
