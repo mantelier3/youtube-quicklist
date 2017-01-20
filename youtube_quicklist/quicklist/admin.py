c0c10859f028be128d9c5ee37a7f8b36e47d6fd7
riba1122 <riba1122@gmail.com>
2017-01-20 04:17:29 +0100

    added models

--- youtube_quicklist/quicklist/admin.py
+++ youtube_quicklist/quicklist/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Playlist, Video
 
 # Register your models here.
+
+
+# admin.site.register(User)
+# admin.site.register(Group)
+
+admin.site.register(Playlist)
+admin.site.register(Video)
